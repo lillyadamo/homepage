239b71c4b456a328a716dfcb44a91f387d8396ab
ladamo-rrc <ladamo@academic.rrc.ca>
2025-05-13 09:31:20 -0500

    Wrapped script in DOM content loader to fix menu not loading on mobile.

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
-const hamburger = document.getElementById('hamburger-btn');
-const nav = document.querySelector('.header-nav');
+document.addEventListener("DOMContentLoaded", () => {
+  // hamburger
+  const hamburger = document.getElementById('hamburger-btn');
+  const nav = document.querySelector('.header-nav');
 
-hamburger.addEventListener('click', () => {
-nav.classList.toggle('active');
- });
+  if (hamburger && nav) {
+    hamburger.addEventListener('click', () => {
+      nav.classList.toggle('active');
+    });
+  }
 
-const closeBtn = document.getElementById('close-btn');
+  const closeBtn = document.getElementById('close-btn');
+  if (closeBtn && nav) {
+    closeBtn.addEventListener('click', () => {
+      nav.classList.remove('active');
+    });
+  }
 
-closeBtn.addEventListener('click', () => {
-  nav.classList.remove('active');
+  // modal
+  const modal = document.getElementById("artModal");
+  const modalImg = document.getElementById("modalImage");
+  const captionText = document.getElementById("caption");
+  const images = document.querySelectorAll(".art-item img");
+  const span = document.getElementsByClassName("close")[0];
+
+  if (modal && modalImg && captionText && images.length > 0) {
+    images.forEach((img) => {
+      img.onclick = function () {
+        modal.style.display = "block";
+        modal.style.opacity = "0";
+        modal.offsetWidth; 
+        modal.style.animation = "fadeIn 0.5s forwards";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+      };
+    });
+  }
+
+  if (span && modal) {
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+  }
 });
-
-// Modal code
-const modal = document.getElementById("artModal");
-const modalImg = document.getElementById("modalImage");
-const captionText = document.getElementById("caption");
-const images = document.querySelectorAll(".art-item img");
-
-images.forEach((img) => {
-  img.onclick = function() {
-    modal.style.display = "block";  
-    modal.style.opacity = "0";      
-    modal.offsetWidth;            
-    modal.style.animation = "fadeIn 0.5s forwards"; 
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt; 
-  };
-});
-
-const span = document.getElementsByClassName("close")[0];
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
