e18ec45c5cbb39e34e7f5d8f4447778fcb3ff6b1
ladamo-rrc <ladamo@academic.rrc.ca>
2026-03-03 10:46:55 -0600

    Updated home.tsx and added routing in app.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom'
 import Header from './components/common/header'
 import Footer from './components/common/footer'
 import Home from './pages/home'
@@ -6,7 +7,9 @@ function App() {
   return (
     <div>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </div>
   )
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,33 @@
 import { Link } from 'react-router-dom'
 
-function Home(){
-
-    return(
-        <section>
-            <h1 className='section-title'>Welcome</h1>
-            <p>My name is lilly</p>
-            <div className='link-album'>
-                <Link className="photo-item" to="/art">
-                    <img src="/img/placeholders/knight-armor.png" alt="Art Preview" />
-                    <span className="photo-label">art</span>
-                </Link>
-            </div>
-        </section>
-    )
+function Home() {
+  return (
+    <section>
+      <h1 className='section-title'>welcome</h1>
+      <p>my name is lilly. I'm currently studying application development and delivery at 
+        Red River Polytechnic. This is a website showcasing some of my work. 
+        I enjoy photography, art, and coding.
+      </p>
+      <div className='photo-album'>
+        <Link className="photo-item" to="/art">
+          <img src="/img/placeholders/knight-armor.png" alt="Art Preview" />
+          <span className="photo-label">art</span>
+        </Link>
+        <Link className="photo-item" to="/photography">
+          <img src="/img/placeholders/photo-1.jpg" alt="Photography Preview" />
+          <span className="photo-label">photography</span>
+        </Link>
+        <Link className="photo-item" to="/projects">
+          <img src="/img/placeholders/coding.png" alt="Projects Preview" />
+          <span className="photo-label">projects</span>
+        </Link>
+        <Link className="photo-item" to="/devlog">
+          <img src="/img/placeholders/spongebob.jpg" alt="Devlog Preview" />
+          <span className="photo-label">devlog</span>
+        </Link>
+      </div>
+    </section>
+  )
 }
 
 export default Home
