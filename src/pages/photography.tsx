d8089a2eaca5c638237cac1beebe95764183114b
ladamo-rrc <ladamo@rrc.ca>
2026-03-07 13:26:30 -0600

    updated photography.

--- src/pages/photography.tsx
+++ src/pages/photography.tsx
@@ -2,6 +2,9 @@ import ImageGrid from "../components/common/gallery/PhotoGrid";
 
 const photographs = [
     {src: "img/photography/espresso-at-nonnas.jpg", alt: "espresso at nonnas", width: 6000, height: 4000}, 
+    {src: "img/photography/cake.jpg", alt: "cake", width: 5152, height: 3864}, 
+    {src: "img/photography/green.jpg", alt: "green", width: 4712, height: 3072}, 
+
 ]
 
 function Photography() {
@@ -10,6 +13,7 @@ function Photography() {
             <h1 className="section-title">photography</h1>
             <p>i enjoy taking photos. The cameras I use are a Ricoh GRIIIx, 
                 a Nikon coolpix, and an iphone on occasion.</p>
+    
             <ImageGrid images={photographs} />
         </section>
     )
