16d2c3b33b82510a91baae49f51f6b78e04adeae
ladamo-rrc <ladamo@rrc.ca>
2026-03-07 13:55:56 -0600

    created projects page.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/home'
 import Art from './pages/art'
 import Photography from './pages/photography'
 import Contact from './pages/contact'
+import Projects from './pages/projects'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/art" element={<Art/>} />
         <Route path="/photography" element={<Photography/>} />
+        <Route path="/projects" element={<Projects/>} />
         <Route path="/contact" element={<Contact/>} />
       </Routes>
       <Footer />
