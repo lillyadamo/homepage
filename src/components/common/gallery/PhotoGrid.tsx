d881624aa823e120af46f0edad21953f05d15a8f
ladamo-rrc <ladamo@rrc.ca>
2026-03-08 09:26:19 -0500

    fixed issue where gallery images were not displaying on mobile.

--- src/components/common/gallery/PhotoGrid.tsx
+++ src/components/common/gallery/PhotoGrid.tsx
@@ -16,22 +16,28 @@ type PhotoGridProps = {
 
 function ImageGrid({ images }:PhotoGridProps){
     const [selectedImage, setSelectedImage] = useState<string | null>(null)
+    console.log("images:", images)
 
     return (
-    <div>
+      <div style={{ width: "100%", minWidth: "100px" }}>
         <PhotoAlbum
-            layout="masonry"
-            photos={images}
-            onClick={({ photo }: { photo: Photo }) => setSelectedImage(photo.src)}
-      />
-      {selectedImage && (
-        <div className="modal" onClick={() => setSelectedImage(null)}>
-          <span className="close">×</span>
-          <img className="modal-content" src={selectedImage} />
-        </div>
-      )}
-    </div>
-  )
+          layout="masonry"
+          photos={images}
+          columns={(containerWidth) => {
+            if (containerWidth < 400) return 1
+            if (containerWidth < 768) return 2
+            return 3
+          }}
+          onClick={({ photo }: { photo: Photo }) => setSelectedImage(photo.src)}
+        />
+        {selectedImage && (
+          <div className="modal" onClick={() => setSelectedImage(null)}>
+            <span className="close">×</span>
+            <img className="modal-content" src={selectedImage} />
+          </div>
+        )}
+      </div>
+    )
 }
 
 export default ImageGrid
