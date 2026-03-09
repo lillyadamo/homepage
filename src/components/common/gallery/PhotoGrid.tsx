b10b9310341015bea4414017e263a5a8af8e5d37
ladamo-rrc <ladamo@rrc.ca>
2026-03-08 23:01:09 -0500

    removed react-photo-album.

--- src/components/common/gallery/PhotoGrid.tsx
+++ src/components/common/gallery/PhotoGrid.tsx
@@ -1,45 +1,37 @@
 import { useState } from "react"
-import PhotoAlbum from "react-photo-album"
-import type { Photo } from "react-photo-album"
-import "react-photo-album/styles.css"
- 
-type Image = {
-    src: string
-    alt: string
-    width: number
-    height: number
+
+export type Image = {
+  src: string
+  alt: string
+  width: number
+  height: number
 }
 
 type PhotoGridProps = {
-    images: Image[]
+  images: Image[]
 }
 
-function ImageGrid({ images }:PhotoGridProps){
-    const [selectedImage, setSelectedImage] = useState<string | null>(null)
-    console.log("images:", images)
+function ImageGrid({ images }: PhotoGridProps) {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null)
 
-    return (
-      <div style={{ width: "100%", minWidth: "100px" }}>
-        <PhotoAlbum
-            layout="masonry"
-            photos={images}
-            columns={(containerWidth) => {
-              if (containerWidth < 400) return 1
-              if (containerWidth < 768) return 2
-              return 3
-            }}
-            defaultContainerWidth={300}
-            onClick={({ photo }: { photo: Photo }) => setSelectedImage(photo.src)}
-          />
-        {selectedImage && (
-          <div className="modal" onClick={() => setSelectedImage(null)}>
-            <span className="close">×</span>
-            <img className="modal-content" src={selectedImage} />
+  return (
+    <div>
+      <div className="masonry-grid">
+        {images.map((image, index) => (
+          <div key={index} className="masonry-item" >
+            <img src={image.src} alt={image.alt} 
+            onClick={() => setSelectedImage(image.src)}/>
           </div>
-        )}
+        ))}
       </div>
-    )
+      {selectedImage && (
+        <div className="modal" onClick={() => setSelectedImage(null)}>
+          <span className="close">×</span>
+          <img className="modal-content" src={selectedImage} />
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default ImageGrid
-
